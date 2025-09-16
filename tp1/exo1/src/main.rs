56582e73926fcd2d414c803cf7f1f352470c824a
Etienne Lozes <elozes@unice.fr>
2025-09-16 08:48:41 +0200

    correction exo1

---
+++ tp1/exo1/src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Usage: {} <name>", args[0]);
+    }
+    // Check for the "-g" flag : la façon simple
+    // let option_g = args.contains("-g")
+    let mut option_g = false;
+    for i in 0..args.len() {
+        if args[i] == "-g" {
+            option_g = true;
+        }
+    }
+    if option_g { dbg!(&args); }
+    for arg in &args[1..] {
+        if arg != "-g" {
+            println!("Bonjour, {}!", arg);
+        }
+    }
+}
+
+// fn main() {
+//     println!("Hello, world!");
+// }
