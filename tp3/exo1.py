7ffcdabc7d56343c90ef47afd2448548bf14feb2
Etienne Lozes <elozes@unice.fr>
2025-10-03 17:34:13 +0200

    debut corrige tp3

---
+++ tp3/exo1.py
@@ -0,0 +1,32 @@
+# Écrivez un programme qui réalise les étapes suivantes:
+# 1 afficher une invite (ex: tapez votre commande: ) et lire l'entrée standard (input en Python)
+# 2 en extraire un tableau args des arguments de la ligne de commande lue en découpant selon les espaces (split en Python)
+# 3 créer un fils qui exécute la commande (fork et execvp)
+# 4 attendre la fin du fils, et revenir au départ. 
+# 
+# Le shell termine lorsque l'entrée standard se termine (Ctrl+D). 
+# Testez votre programme en exécutant deux-trois commandes 
+# (ex: ls, puis echo hello world, puis ps -aux)
+# Ajoutez la gestion d'erreur: lorsque la commande tapée par 
+# l'utilisateur n'est pas valide (échec de execvp), affichez un message 
+# d'erreur et une nouvelle invite.
+
+import os, sys
+
+while True:
+    try:
+        cmd_line = input("tapez votre commande: ")
+    except EOFError: # <- si la sortie a été fermée, input lève une exception
+        sys.exit()
+    args = cmd_line.split(" ")
+    fork_result = os.fork()
+    if fork_result == 0:
+        try:
+            os.execvp(args[0], args)
+            # partie de code non ateignable, le fils a changé de code, il terminera avec un exit dans son nouveau code
+            assert(false)
+        except:
+            print(f"microsh: command not found: {args[0]}", file=sys.stderr)
+            sys.exit(127)
+
+    os.wait()
---
+++ tp3/exo3.py
@@ -0,0 +1,58 @@
+import os, sys
+
+def exec_cmd(cmd_line):
+    args = cmd_line.split(" ")
+    if os.fork() == 0:
+        try:
+            os.execvp(args[0], args)
+        except:
+            print("command not found", file=sys.stderr)
+            sys.exit(127)
+    _pid, status = os.wait()
+    return status
+
+internal_cmds = ["exit", "cd", "env", "history"]
+
+def exec_internal_cmd(cmd_line):
+    global history
+    args = cmd_line.split(" ")
+    if args[0] == "exit":
+        try:
+            code_de_sortie = int(args[1])
+        except:
+            code_de_sortie = 0
+        sys.exit(code_de_sortie)
+    if args[0] == "cd":
+        try:
+            path = args[1]
+        except:
+            path = os.environ["HOME"]
+        try:
+            os.chdir(path)
+        except:
+            print("no such directory", file=sys.stderr)
+            return 1
+    if args[0] == 'env':
+        for varid, val in os.environ.items():
+            print(f"{varid}={val}")
+    if args[0] == "history":
+        for i in range(len(history)):
+            print(f"{i}  {history[i]}")
+    return 0
+
+history = []
+while True:
+    try:
+        cmd_line = input("tapez votre commande: ")
+    except EOFError: # l'entrée standard est fermée
+        sys.exit()
+    history.append(cmd_line)
+    for simple_cmd in cmd_line.split("&&"):
+        simple_cmd = simple_cmd.strip()
+        arg0 = simple_cmd.split(" ",1)[0]
+        if arg0 in internal_cmds:
+            status = exec_internal_cmd(simple_cmd)
+        else:
+            status = exec_cmd(simple_cmd)
+        if status != 0:
+            break
